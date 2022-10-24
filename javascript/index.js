3f1e6059a5dede7aeba917590324069cd8b70bff
unknown <shzaheer514@gmail.com>
2022-10-24 13:03:20 +0530

    added side nav animation

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,17 @@ document.addEventListener("DOMContentLoaded",function(){
 
     let open_menu = document.getElementById("open");
     let transition_ele  = document.querySelector(".div-rotate");
-    console.log(transition_ele);
-    console.log(open_menu);
-    open_menu.addEventListener("click",function(e){
+    let animate = document.querySelectorAll(".common")
+    
+        //    rotating div
+        open_menu.addEventListener("click",function(e){
         e.preventDefault();
         transition_ele.classList.toggle("transition-class");
-   
+        // animation for nav
+        animate.forEach(function(obj){
+            obj.classList.toggle("animation")
+        });
+
     });
 
 
